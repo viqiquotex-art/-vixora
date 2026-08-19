8cbc9fa7f97000b0b76e5c60b8d1b292ed705a22
viqiquotex-art <viqiquotex@gmail.com>
2026-08-19 21:17:47 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -64,7 +64,7 @@ export default {
 
       // Gemini API
       const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent",
         {
           method: "POST",
 
