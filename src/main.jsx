424d28fe011908ad3b86eeb029369de59b1da719
Bless <viqiquotex@gmail.com>
2026-09-05 13:21:19 +0700

    fix: use single clean VIXORA stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
-import './vixora-overrides.css'
+import './vixora.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
